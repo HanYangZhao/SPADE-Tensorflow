ae1fdbb4b2430c05ca9462bae95776650d816dfe
julesd16 <julesd@momentfactory.com>
2020-08-07 16:52:32 -0400

    fixed download coco.sh

--- download_coco.sh
+++ download_coco.sh
@@ -4,14 +4,15 @@ mkdir -p dataset/coco/
 
 curl -o coco_train2017.zip -L http://images.cocodataset.org/zips/train2017.zip
 unzip coco_train2017.zip 
-mv coco_train2017/ dataset/coco/image
+mv train2017/ dataset/coco/image
 
 curl -o coco_segmap.zip -L https://github.com/HanYangZhao/SPADE-Tensorflow/releases/download/aed20k/coco_segmap.zip
+unzip coco_segmap.zip
 mv segmap dataset/coco/segmap
 
 curl -o coco_val2017.zip -L http://images.cocodataset.org/zips/val2017.zip
 unzip coco_val2017.zip
-mv coco_val2017/ dataset/coco/val
+mv val2017/ dataset/coco/val
 
 curl -o coco_segmap_test.zip -L https://github.com/HanYangZhao/SPADE-Tensorflow/releases/download/aed20k/coco_segmap_test.zip
 unzip coco_segmap_test.zip
